a67aa042c441eeeedd7fa4c6462941542c53189c
Kaboudou <kingamet91@gmail.com>
2020-03-31 16:24:03 +0200

    Edit Shop

---
+++ src/components/Date.vue
@@ -0,0 +1,55 @@
+<template>
+
+        <div class="flex flex-col w-56 m-auto my-4">
+            <div class="flex flex-wrap -mx-3 mb-6 ">
+                <div class="w-full md:w-1/6 px-3 px:w-1/3 md:mb-0 ">
+                    <label class="font-semibold text-gray-700 block mb-2 align-middle">{{label}}</label>
+                </div>
+                <div class="w-full md:w-1/6 px-0 px:w-1/3 md:mb-0 ">
+                </div>
+                <div class="w-full md:w-2/6 px-1 px:w-1/3  md:mb-0">
+            <input
+                    :key="field1"
+                    :type="type"
+                    :name="field1"
+                    :value="value"
+                    class="block appearance-none w-full bg-white border border-grey-light text-left hover:border-grey py-2 px-4 rounded shadow"
+                    @change="handleInput($event)"
+            />
+                </div>
+                <div class="w-full md:w-2/6 px-1 px:w-1/3">
+            <input
+                    :key="field2"
+                    :type="type"
+                    :name="field2"
+                    :value="value"
+                    class="block appearance-none w-full bg-white border border-grey-light text-left hover:border-grey px-2 py-2 rounded shadow "
+                    @change="handleInput($event)"
+            />
+                </div>
+            </div>
+        </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Date",
+        props: ["label","type", "field1","field2", "value", "checked"],
+        inject: ["setValue"],
+        methods: {
+            handleInput: function (event) {
+                this.setValue({
+                    type: this.type,
+                    key: this.name,
+                    value: event.target.value,
+                    isChecked: event.target.checked
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/EditShop.vue
@@ -0,0 +1,117 @@
+<template>
+    <v-component>
+    <div class="flex flex-col w-56 m-auto my-4">
+        <h1 class="label-forms mb-5 mt-10 ">
+            Editer mon enseigne
+        </h1>
+    </div>
+    <Formik>
+        <FormGroup v-for="field in fields"
+                   :key="field.name"
+                   :type="field.type"
+                   :name="field.name"
+                   :value="field.value"
+                   :label="field.label"
+        >
+        </FormGroup>
+
+        <Date v-for="date in dates"
+                   :key="date.name"
+                   :type="date.type"
+                   :name="date.name"
+                   :value="date.value"
+                   :label="date.label"
+        >
+        </Date>
+
+    </Formik>
+    </v-component>
+
+</template>
+
+<script>
+    import Formik from "../components/Formik/Formik";
+    import FormGroup from "../components/Formik/FormGroup";
+    import Date from "../components/Date";
+    export default {
+        name: "Editshop",
+        components: {Formik, FormGroup, Date},
+        data: function () {
+            return {
+                fields: [
+                    {
+                        label: 'Nom',
+                        name: 'name',
+                        type: 'text',
+                    },
+                    {
+                        label: 'Adresse',
+                        name: 'address',
+                        type: 'text',
+                    },
+                    {
+                        label: 'Intervalle des créneaux (minutes)',
+                        name: 'interval',
+                        type: 'number',
+                    },
+                    {
+                        label: 'Nombre de personnes par créneau',
+                        name: 'number_max',
+                        type: 'number',
+                    },
+
+
+                ],
+                dates: [
+                    {
+                        label: 'lundi',
+                        field1: 'begin_lun',
+                        field2: 'end_lun',
+                        type: 'time',
+                    },
+                    {
+                        label: 'Mardi',
+                        field1: 'begin_mar',
+                        field2: 'end_mar',
+                        type: 'time',
+                    },
+                    {
+                        label: 'Mercredi',
+                        field1: 'begin_mer',
+                        field2: 'end_mer',
+                        type: 'time',
+                    },
+                    {
+                        label: 'Jeudi',
+                        field1: 'begin_jeu',
+                        field2: 'end_jeu',
+                        type: 'time',
+                    },
+                    {
+                        label: 'Vendredi',
+                        field1: 'begin_ven',
+                        field2: 'end_ven',
+                        type: 'time',
+                    },
+                    {
+                        label: 'Samedi',
+                        field1: 'begin_sam',
+                        field2: 'end_sam',
+                        type: 'time',
+                    },
+                    {
+                        label: 'Dimanche',
+                        field1: 'begin_dim',
+                        field2: 'end_dim',
+                        type: 'time',
+                    },
+
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
