109e69656478709fe94661bc7dfdaa5d70438ef4
Alex TEA <alextea2@gmail.com>
2020-04-01 00:27:10 +0200

    List Shop

--- src/views/ListShop.vue
+++ src/views/ListShop.vue
@@ -1,67 +1,26 @@
 <template>
-  <div class="mx-auto ">
-    <h1 class="font-hairline mb-6 text-left p-20 pb-10 font-bold text-2xl ">Établissements à proximité</h1>
-
-   <input class="w-1/2 h-16 px-3 border px-4 rounded mb-8 focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg " 
-   type="search" placeholder="Rechercher votre commerce...">
-<div class="max-w-sm w-full lg:max-w-full lg:flex">
-  <div class="h-48 lg:h-auto lg:w-48 flex-none  overflow-hidden" >
-
-   
-  </div>
-  
-       <div class="md:flex bg-gray-200 rounded-lg p-6">
-   <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6" src="avatar.jpg">
-   <div class="text-center md:text-left">
-     <h2 class="text-lg text-teal-600">Carrefour</h2>
-     <div class="text-gray-700">1 rue blabla</div>
-     <div class="text-gray-600">Paris (75001)</div>
-   </div>  
-   </div>
-
-       <div class="md:flex bg-gray-200 rounded-lg p-6 border-collapse ">
-          <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 90 100"><g data-name="Layer 2"><g data-name="clock" color="green"><rect fill="currentColor"  width="120" height="120" transform="rotate(180 12 12)" opacity="0" /><path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8 8 0 0 1-8 8z"/><path d="M16 11h-3V8a1 1 0 0 0-2 0v4a1 1 0 0 0 1 1h4a1 1 0 0 0 0-2z"/></g></g></svg>
-
-   <table class="border-collapse w-full">
-  <thead class="">
-    <tr class="">
-      <th class="px-4 py-2 text-teal-600 ">Lundi</th>
-      <th class="px-4 py-2 text-teal-600">Mardi</th>
-      <th class="px-4 py-2 text-teal-600">Mercredi</th>
-      <th class="px-4 py-2 text-teal-600 ">Jeudi</th>
-      <th class="px-4 py-2 text-teal-600 ">Vendredi</th>
-      <th class="px-4 py-2 text-teal-600 ">Samedi</th>
-      <th class="px-4 py-2 text-teal-600 ">Dimanche</th>
-    </tr>
-
-  </thead>
-
-  <tbody>
-
-          <tr class="">
-
-
-      <td class="border px-4 py-2 ">9h</td>
-      <td class="border px-4 py-2">9H</td>
-      <td class="border px-4 py-2">9H</td>
-    </tr>
-       <tr class="">
-      <td class="border px-4 py-2 ">17h</td>
-      <td class="border px-4 py-2">17H</td>
-      <td class="border px-4 py-2">17H</td>
-    </tr>
-
-    </tbody>
-   </table>
-           <svg width="50" height="200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="chevron-right"><rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0"/><path d="M10.5 17a1 1 0 0 1-.71-.29 1 1 0 0 1 0-1.42L13.1 12 9.92 8.69a1 1 0 0 1 0-1.41 1 1 0 0 1 1.42 0l3.86 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-.7.32z"/></g></g></svg>
-
-   </div>
-
-</div>
-    
-
-   
-   
+  <div class="w-full xl:w-2/3 mx-auto">
+    <h1 class="text-left font-bold text-2xl text-center py-12">
+      Enseignes à proximité
+    </h1>
+    <input class="w-5/6 sm:w-3/4 lg:w-2/3 mx-auto block p-4 border rounded mb-8 focus:outline-none focus:shadow-outline text-base shadow-lg "
+      type="search"
+      placeholder="Rechercher votre commerce..."
+    />
+    <div class="w-full mx-auto lg:">
+      <div class="mx-auto flex justify-center mb-4">
+        <ShopItem></ShopItem>
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+import ShopItem from "../components/ShopItem";
+
+export default {
+  components: {
+    ShopItem
+  }
+};
+</script>
