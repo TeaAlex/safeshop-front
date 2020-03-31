662058531783bcb3f304d2bb9fa1d32eaba426b5
OmarAbdallaa <omar.abdallaa@gmail.com>
2020-03-31 16:09:56 +0200

    fix shop list
    Signed-off-by: OmarAbdallaa <omar.abdallaa@gmail.com>

--- src/views/ListShop.vue
+++ src/views/ListShop.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="mx-auto">
-    <h1 class="font-hairline mb-6 text-left p-20 pb-10 font-bold text-2xl ">Etablissements à proximité</h1>
+  <div class="mx-auto ">
+    <h1 class="font-hairline mb-6 text-left p-20 pb-10 font-bold text-2xl ">Établissements à proximité</h1>
 
    <input class="w-1/2 h-16 px-3 border px-4 rounded mb-8 focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg " 
    type="search" placeholder="Rechercher votre commerce...">
@@ -15,15 +15,14 @@
    <div class="text-center md:text-left">
      <h2 class="text-lg text-teal-600">Carrefour</h2>
      <div class="text-gray-700">1 rue blabla</div>
-     <i class="eva eva-github"></i>
-
      <div class="text-gray-600">Paris (75001)</div>
    </div>  
    </div>
 
-       <div class="md:flex bg-gray-200 rounded-lg p-6">
+       <div class="md:flex bg-gray-200 rounded-lg p-6 border-collapse ">
+          <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 90 100"><g data-name="Layer 2"><g data-name="clock" color="green"><rect fill="currentColor"  width="120" height="120" transform="rotate(180 12 12)" opacity="0" /><path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8 8 0 0 1-8 8z"/><path d="M16 11h-3V8a1 1 0 0 0-2 0v4a1 1 0 0 0 1 1h4a1 1 0 0 0 0-2z"/></g></g></svg>
 
-   <table class="table-auto ">
+   <table class="border-collapse w-full">
   <thead class="">
     <tr class="">
       <th class="px-4 py-2 text-teal-600 ">Lundi</th>
@@ -34,9 +33,14 @@
       <th class="px-4 py-2 text-teal-600 ">Samedi</th>
       <th class="px-4 py-2 text-teal-600 ">Dimanche</th>
     </tr>
+
   </thead>
+
   <tbody>
+
           <tr class="">
+
+
       <td class="border px-4 py-2 ">9h</td>
       <td class="border px-4 py-2">9H</td>
       <td class="border px-4 py-2">9H</td>
@@ -46,8 +50,11 @@
       <td class="border px-4 py-2">17H</td>
       <td class="border px-4 py-2">17H</td>
     </tr>
+
     </tbody>
    </table>
+           <svg width="50" height="200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="chevron-right"><rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0"/><path d="M10.5 17a1 1 0 0 1-.71-.29 1 1 0 0 1 0-1.42L13.1 12 9.92 8.69a1 1 0 0 1 0-1.41 1 1 0 0 1 1.42 0l3.86 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-.7.32z"/></g></g></svg>
+
    </div>
 
 </div>
