fbc9a0cf8856b34c66978ca2624d1330f82ae8be
OmarAbdallaa <omar.abdallaa@gmail.com>
2020-03-31 15:14:17 +0200

    shop list
    Signed-off-by: OmarAbdallaa <omar.abdallaa@gmail.com>

---
+++ src/views/ListShop.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="mx-auto">
+    <h1 class="font-hairline mb-6 text-left p-20 pb-10 font-bold text-2xl ">Etablissements à proximité</h1>
+
+   <input class="w-1/2 h-16 px-3 border px-4 rounded mb-8 focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg " 
+   type="search" placeholder="Rechercher votre commerce...">
+<div class="max-w-sm w-full lg:max-w-full lg:flex">
+  <div class="h-48 lg:h-auto lg:w-48 flex-none  overflow-hidden" >
+
+   
+  </div>
+  
+       <div class="md:flex bg-gray-200 rounded-lg p-6">
+   <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6" src="avatar.jpg">
+   <div class="text-center md:text-left">
+     <h2 class="text-lg text-teal-600">Carrefour</h2>
+     <div class="text-gray-700">1 rue blabla</div>
+     <i class="eva eva-github"></i>
+
+     <div class="text-gray-600">Paris (75001)</div>
+   </div>  
+   </div>
+
+       <div class="md:flex bg-gray-200 rounded-lg p-6">
+
+   <table class="table-auto ">
+  <thead class="">
+    <tr class="">
+      <th class="px-4 py-2 text-teal-600 ">Lundi</th>
+      <th class="px-4 py-2 text-teal-600">Mardi</th>
+      <th class="px-4 py-2 text-teal-600">Mercredi</th>
+      <th class="px-4 py-2 text-teal-600 ">Jeudi</th>
+      <th class="px-4 py-2 text-teal-600 ">Vendredi</th>
+      <th class="px-4 py-2 text-teal-600 ">Samedi</th>
+      <th class="px-4 py-2 text-teal-600 ">Dimanche</th>
+    </tr>
+  </thead>
+  <tbody>
+          <tr class="">
+      <td class="border px-4 py-2 ">9h</td>
+      <td class="border px-4 py-2">9H</td>
+      <td class="border px-4 py-2">9H</td>
+    </tr>
+       <tr class="">
+      <td class="border px-4 py-2 ">17h</td>
+      <td class="border px-4 py-2">17H</td>
+      <td class="border px-4 py-2">17H</td>
+    </tr>
+    </tbody>
+   </table>
+   </div>
+
+</div>
+    
+
+   
+   
+  </div>
+</template>
+
