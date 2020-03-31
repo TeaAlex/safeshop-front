58b048eddce9ca31351c9fbef7d42af8f39c2366
Alex TEA <alextea2@gmail.com>
2020-04-01 00:27:10 +0200

    List Shop

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,34 +1,27 @@
 <template>
-<body class="antialiased bg-gray-200">
-  <header class="lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2">
+  <header class="lg:px-16 px-6 bg-white flex flex-wrap items-center py-4">
     <div class="flex-1 flex justify-between items-center">
-      <a href="/">
-    <h1 class="block titre mt-3 pl-6">SAFE<span class="text-teal-600">SHOP</span></h1>
-    </a>
-  </div>
+      <a href="/" class="block titre">SAFE<span class="text-teal-600">SHOP</span></a>
+    </div>
 
-   <label for="menu-toggle" class="pointer-cursor lg:hidden block"><svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
-  <input class="hidden" type="checkbox" id="menu-toggle" />
+    <label for="menu-toggle" class="pointer-cursor lg:hidden block"><svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
+    <input class="hidden" type="checkbox" id="menu-toggle" />
 
-  <div class="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
-    <nav>
-      <ul class="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
-        <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-teal-600" href="/reservation">Réservation</a></li>
-        <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-teal-600" href="/plannig">Planning</a></li>
-        <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-teal-600" href="/connexion">Connexion</a></li>
-        <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-teal-600 lg:mb-0 mb-2" href="/inscription">Inscription</a></li>
-      </ul>
-    </nav>
-    <a href="#" class="lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor">
-      <img class="rounded-full w-10 h-10 border-2 border-transparent hover:border-teal-600" src="https://pbs.twimg.com/profile_images/1128143121475342337/e8tkhRaz_normal.jpg" alt="Andy Leverenz">
-    </a>
+    <div class="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
+      <nav>
+        <ul class="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+          <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-teal-600" href="/reservation">Réservation</a></li>
+          <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-teal-600" href="/plannig">Planning</a></li>
+          <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-teal-600" href="/connexion">Connexion</a></li>
+          <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-teal-600 lg:mb-0 mb-2" href="/inscription">Inscription</a></li>
+        </ul>
+      </nav>
+      <a href="#" class="lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor">
+        <img class="rounded-full w-10 h-10 border-2 border-transparent hover:border-teal-600" src="https://pbs.twimg.com/profile_images/1128143121475342337/e8tkhRaz_normal.jpg" alt="Andy Leverenz">
+      </a>
 
-  </div>
-
+    </div>
   </header>
-  
-</body>
-
 </template>
 
 <script>
