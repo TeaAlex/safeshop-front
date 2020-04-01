fac29d36ef8ceab00fa0e86919264633df67eeb0
Kaboudou <kingamet91@gmail.com>
2020-04-01 15:05:55 +0200

    Edit Shop + route

--- src/components/Date.vue
+++ src/components/Date.vue
@@ -1,32 +1,29 @@
 <template>
 
-        <div class="flex flex-col w-56 m-auto my-4">
+        <div class="flex flex-col w-2/3 items-stretch m-auto my-4">
             <div class="flex flex-wrap -mx-3 mb-6 ">
                 <div class="w-full md:w-1/6 px-3 px:w-1/3 md:mb-0 ">
                     <label class="font-semibold text-gray-700 block mb-2 align-middle">{{label}}</label>
                 </div>
-                <div class="w-full md:w-1/6 px-0 px:w-1/3 md:mb-0 ">
-                </div>
-                <div class="w-full md:w-2/6 px-1 px:w-1/3  md:mb-0">
+
             <input
                     :key="field1"
                     :type="type"
                     :name="field1"
                     :value="value"
-                    class="block text-sm appearance-none w-full bg-white border border-grey-light text-left hover:border-grey py-2 px-1 rounded shadow"
+                    class="block text-sm appearance-none w-full  bg-white border border-grey-light text-left hover:border-grey py-2 px-1 md:w-1/6  px:w-1/3 rounded shadow"
                     @change="handleInput($event)"
             />
-                </div>
-                <div class="w-full md:w-2/6 px-1 px:w-1/3">
+
             <input
                     :key="field2"
                     :type="type"
                     :name="field2"
                     :value="value"
-                    class="block text-sm appearance-none w-full bg-white border border-grey-light text-left hover:border-grey px-1 py-2 rounded shadow "
+                    class="block text-sm appearance-none w-full bg-white border border-grey-light text-left hover:border-grey px-1 py-2 md:w-1/6  px:w-1/3 rounded shadow "
                     @change="handleInput($event)"
             />
-                </div>
+
             </div>
         </div>
 
---
+++ src/components/Formik/FormGroup2.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <div class="flex-col w-2/3 m-auto my-4">
+    <label class="font-semibold text-gray-700 text-left block mb-2">{{ label }}</label>
+    <Field :key="name"
+           :type="type"
+           :name="name"
+           :value="value"
+           class="bg-grey px-3"
+    >
+      <slot></slot>
+    </Field>
+  </div>
+
+
+</template>
+
+<script>
+  import Field from './Field'
+  export default {
+    name: "FormGroup2",
+    components: {
+      Field
+    },
+    props: ['type', 'label', 'name', 'value','style']
+  }
+</script>
+
+<style scoped>
+
+</style>
