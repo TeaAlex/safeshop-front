98945470e6cd750a74e1121cdcf7c41d0d7cfbb9
Kaboudou <kingamet91@gmail.com>
2020-04-02 11:51:25 +0200

    Edit Shop + route

--- src/components/Date.vue
+++ src/components/Date.vue
@@ -1,9 +1,9 @@
 <template>
 
         <div class="flex flex-col w-2/3 items-stretch m-auto my-4">
-            <div class="flex flex-wrap -mx-3 mb-6 ">
-                <div class="sm:w-1/4 md:w-1/4 px-3 mr-2 md:mb-2 ">
-                    <label class="font-semibold text-gray-700 block mb-2  align-middle">{{label}}</label>
+            <div class="flex flex-wrap -mx-3 mb-6  justify-end  sm:justify-end md:justify-between lg:justify-between xl:justify-between ">
+                <div class="sm:w-1/4 md:w-1/4  px-2 md:mb-2 -mr-1 items-right  ">
+                    <label class="font-semibold text-gray-700 block mb-2 sm:text-sm align-middle">{{label}}</label>
                 </div>
 
             <input
@@ -11,7 +11,7 @@
                     :type="type"
                     :name="field1"
                     :value="value1"
-                    class="block text-sm appearance-none  bg-white border border-grey-light text-left hover:border-grey py-2 px-2 sm:w-1/4 md:w-1/4 mr-2 items-center rounded shadow"
+                    class="block text-sm appearance-none  bg-white border border-grey-light text-left hover:border-grey py-2 px-2 sm:w-1/4 md:w-1/4  items-left rounded shadow"
                     @change="handleInput($event)"
             />
 
@@ -20,7 +20,7 @@
                     :type="type"
                     :name="field2"
                     :value="value2"
-                    class="block text-sm appearance-none bg-white border border-grey-light text-left hover:border-grey px-2 py-2 sm:w-1/4 md:w-1/4 ml-2 align-right rounded shadow "
+                    class="block text-sm appearance-none bg-white border border-grey-light text-left hover:border-grey px-2 py-2 sm:w-1/4 md:w-1/4 ml-2 sm:ml-1 items-left rounded shadow "
                     @change="handleInput($event)"
             />
 
