59277fcc799417c4b84e863b671655863220a6f7
Soufian AIT TIRITE <contact@soufian-a.net>
2020-04-03 21:47:31 +0200

    add component for validateEmail

---
+++ src/views/ValidateEmail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ValidateEmail",
+  components: { },
+  data: function() {
+    return {
+    };
+  },
+  computed: {
+    status() {
+      return this.$store.state.users.validated;
+    }
+  },
+  created(){
+    this.$store.dispatch('users/validate_mail',{user:{token:this.$route.params.id}})
+    console.log(this.$store.state.users.validated);
+  }
+};
+</script>
