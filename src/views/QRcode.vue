032261a6e924b754c0f036789dfac32680b23eb7
OmarAbdallaa <omar.abdallaa@gmail.com>
2020-04-03 14:19:48 +0200

    fix qr
    Signed-off-by: OmarAbdallaa <omar.abdallaa@gmail.com>

--- src/views/QRcode.vue
+++ src/views/QRcode.vue
@@ -12,9 +12,7 @@
 </template>
 
 <script>
-    import jsPDF from "jspdf";
     import Vue from 'vue';
-    import html2canvas from "html2canvas"
     import QrcodeVue from 'qrcode.vue';
     import Print from 'vue-print-nb'
     import ShopItem from "../components/ShopItem";
@@ -38,27 +36,6 @@
             ShopItem
         },
         methods: {
-            downloadWithCSS() {
-                const doc = new jsPDF();
-                /** WITH CSS */
-                var canvasElement = document.createElement('canvas');
-                html2canvas(this.$refs.content, {
-                    canvas: canvasElement
-                }).then(function (canvas) {
-                    const img = canvas.toDataURL("image/jpeg", 0.8);
-                    doc.addImage(img, 'JPEG', 50, 50);
-                    doc.save("sample.pdf");
-                });
-            },
-            download() {
-                const doc = new jsPDF();
-                /** WITHOUT CSS */
-                const contentHtml = this.$refs.content.innerHTML;
-                doc.fromHTML(contentHtml, 100, 100, {
-                    width: 1700
-                });
-                doc.save("sample.pdf");
-            },
             print() {
                 const modal = document.getElementById("content")
                 const cloned = modal.cloneNode(true)
