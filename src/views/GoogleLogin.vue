882cd1518625d53dbd9535010f0dcc6173657d90
Soufian AIT TIRITE <contact@soufian-a.net>
2020-04-08 21:35:56 +0200

    fix on login with Google for User state

--- src/views/GoogleLogin.vue
+++ src/views/GoogleLogin.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: "GoogleLogin",
   components: { },
@@ -13,6 +14,22 @@ export default {
   created(){
     localStorage.setItem("userToken", this.$route.params.id);
     this.$store.commit('users/setIsLogged', true);
+    const url = process.env.VUE_APP_BASE_URL;
+    new Promise((resolve,reject) => {
+      axios.get(`${url}/user/current-user`, {
+        headers: {
+          Authorization: `Bearer ${this.$route.params.id}`
+        }
+      })
+      .then((res)=>{
+        const {user} = res.data;
+        this.$store.state.users.user = user;
+        resolve();
+      })
+      .catch((error) => {
+        reject(error);
+      })
+    })
     this.$router.push('/')
   }
 };
